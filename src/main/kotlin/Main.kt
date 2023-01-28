1e04e3dca1879449f03772019e1f49ba580be1e5
Donghui <eastshine0221@gmail.com>
2023-01-28 17:17:09 +0900

    Fold Example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val numbers = listOf(1, 2, 3, 4)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    //with for loop
+    var resultWithForLoop = 1
+    for (number in numbers) {
+        resultWithForLoop *= number
+    }
+    println(resultWithForLoop)
+
+    // with forEach
+    var resultWithForEach = 1
+    numbers.forEach { resultWithForEach *= it }
+    println(resultWithForEach)
+
+    val resultWithFold = numbers.fold(1) { result, next -> result * next }
+    println(resultWithFold)
 }
